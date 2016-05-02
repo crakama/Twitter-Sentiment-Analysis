a1279dcc7d66e5c959b95983e55051d78d4e5ff6
crakama <crakama89@gmail.com>
2016-05-02 15:01:12 +0300

    Prepare virtul environment and set up project structure

---
+++ consumer_key.py
@@ -0,0 +1,9 @@
+Consumer Key (API Key)	BAKDTiFR9tvGl9HwUqHn3VfwW
+
+Consumer Secret (API Secret)	VD0CBzlxxMCV3ya746drzG9Z9VLWp55YuOxmCMunmIN8E154iv
+
+
+Access Token	324805546-8vOSeTDQQLsSZhPh1pNI1eELsU0MCU7E3fUBdbbh
+
+Access Token Secret	wZ8vNzKe5K7n3YyZ7LKITFtmveRtHQ2s2CTYHNMGdjNBc
+
