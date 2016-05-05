6d62e69b27e75fb64248cfef66866627aa3b98f5
crakama <crakama89@gmail.com>
2016-05-05 17:00:43 +0300

    Fix Alchemy Sentiment Analysis API bug

--- twitment/search.py
+++ twitment/search.py
@@ -1,28 +1,44 @@
-
+import os
 import json
 import oauth
 import requests
 import consumer_key
-import nltk
-from nltk.corpus import stopwords
+from stop_words import StopWords
 from alchemyapi import AlchemyAPI
 from collections import Counter
 from time import sleep
 from tqdm import tqdm
 from prettytable import PrettyTable
 
+file_path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(file_path)
 
 class ClassTwitter(object):
 
     def __init__(self):
 
         pass
+    # import ipdb; ipdb.set_trace()
+    def stop_words(self, dirtywords):
+        """ Remove the stop words  """
+        # stopwords = open('stop_words.txt', 'r').read().split('')
+        with open(os.path.join(path, 'stop_words.txt'), "r+") as f:
+            data = f.readlines()
+
+        cleanwords = []
+        dirt = []
+        for word in dirtywords:
+            if word.lower() in data:
+                dirt.append(word)
+            else:
+                cleanwords.append(word)
 
     def wordFrequency(self, wordslist):
         """
-        Performs word frequncy,reads data stored in the json file and display it
+            Performs word frequncy,reads data stored in the json file
+            and display it
         """
-        limit = 10
+        limit = 20
         dict_ = {}
         """ Perform word frequency """
 
@@ -44,14 +60,16 @@ class ClassTwitter(object):
         data = json.load(data_file)
         data_file.close()
 
-
-
+        # import ipdb; ipdb.set_trace()
         prettytable = PrettyTable(field_names=["Words", 'Word Frequency'])
-        counter_ = list(Counter(data).items())
+        counter_ = Counter(data)
 
-        
-
-
+        newList = []
+        lst = list(counter_)
+        for key in lst:
+            for row in key:
+                newList.append(row)
+        lis_ = self.stop_words(str(newList))
 
         """
            Count function has inbuild method-most_common,
@@ -59,35 +77,18 @@ class ClassTwitter(object):
 
         """
         table = [prettytable.add_row(row)
-                 for row in counter_[:limit]]
+                 for row in counter_.most_common()[:limit]]
         prettytable.add_column("Rankings", [i+1 for i in range(len(table))])
         prettytable.align["Words"], prettytable.align[
             'Word Frequency'] = 'l', 'r'
         print(prettytable)
+
         # import ipdb; ipdb.set_trace()
 
-        newList = []
-        for key in counter_:
-            for row in key:
-                newList.append(row)
-        lis_ = str(newList)
+        self.sentimentanalysis(dict_)
 
-        stopwords(lis_)
+    def sentimentanalysis(self, text_):
 
-
-
-        self.sentimentanalysis(lis_)
-
-    def stopwords(self, words):
-        """ Remove the stop words  """
-
-        stops = set(stopwords.words('english'))
-        for word in counter_:
-            for w in word.split():
-                if w.lower() not in stops:
-                    print w
-
-    def sentimentanalysis(self,text_):
         """
            Does sentiment analysis using SQLAlchemy API
         """
@@ -95,7 +96,6 @@ class ClassTwitter(object):
         response = alchemyapi.sentiment("text", text_)
 
         print "Sentiment: ", response["docSentiment"]["type"]
-
 
     def search(self, query):
 
@@ -109,8 +109,8 @@ class ClassTwitter(object):
             for key in tqdm(range(range_)):
                 sleep(0.01)
                 search_results = twitter_.search.tweets(
-                    q=query, lang='en', result_type='recent', screen_name=query)
-                # print search_results
+                    q = query, lang = 'en', result_type = 'recent',screen_name = query)
+
             # import ipdb;ipdb.set_trace()
 
             '''
